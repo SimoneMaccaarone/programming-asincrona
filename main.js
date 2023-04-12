a714467384b77ef6e19e9479caf3ace5ff743f7c
Simone Maccarone <80923893+SimoneMaccaarone@users.noreply.github.com>
2023-04-12 13:25:23 +0200

    c

---
+++ main.js
@@ -0,0 +1,32 @@
+// console.log('async')
+
+
+// setTimeout(callBackTogliLaLasagna, 3000);
+
+// function callBackTogliLaLasagna() {
+//     console.log('Le lasagne sono pronte!')
+// }
+
+
+
+
+
+// setTimeout(callBackButtaLaSpazzatura,10000)
+// function callBackButtaLaSpazzatura(){
+// console.log('BUTTA LA SPAZZATURA!!!!')
+// }
+
+
+// setTimeout(callBacksalutaLaNonna,5000)
+
+// function salutaLaNonna() {
+//     document.getElementById('main').innerHTML=`<h1> CIAO NONNAAAA'</h1>`
+// }
+
+
+// function salutaJing(){
+//     console.log('ciao')
+// }
+
+
+
